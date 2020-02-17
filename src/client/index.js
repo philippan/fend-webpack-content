4c4a134ebdd22123f25abd88bc08f71f95e6a2f5
Philip <philipspan@gmail.com>
2020-02-17 14:57:51 -0800

    Added loaders eslint, file-loader, and json-loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,5 @@
-alert('Big penis!');
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+import img from './img/file.png';
+
+console.log(checkForName);
