85d3fdacfc39b922d098d37a85c4ed126a25cb1f
Philip <philipspan@gmail.com>
2020-02-18 09:23:49 -0800

    Added webpack config.js because there was a Error in finding the module with .src

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const webpack = require('webpack');
+const json = require('./src/client/js/file.json');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+
+module.exports = {
+
+        mode: 'development',
+        entry: './src/client/index.js',
+        module: {
+            
+            rules: [
+                    {
+                        enforce: 'pre',
+                        test: '/\.js$/',
+                        exclude: /node_modules/,
+                        loader: "eslint-loader"
+                    },
+                    {
+                        test: '/\.js$/',
+                        exclude: /node_modules/,
+                        loader: "babel-loader",
+                    },
+                    {
+                        test: /\.(png|jpe?g|gif)$/i,
+                        exclude: /node_modules/,
+                        use: [
+                            {
+                                loader: 'file-loader',
+                            }
+                        ]
+                    },
+                    {
+                        test: '/\.json$/',
+                        loader: "json-loader"
+                    }
+
+            ]
+
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+
+                template: './src/client/views/index.html',
+                filename: './index.html',
+
+            }),
+            new CleanWebpackPlugin({
+            
+                // Simulate the removal of files
+                dry: true,
+                // Write Logs to Console
+                verbose: true,
+                // Automatically remove all unused webpack assets on rebuild
+                cleanStaleWebpackAssets: true,
+                protectWebpackAssets: false
+
+            })
+        ]
+
+};
