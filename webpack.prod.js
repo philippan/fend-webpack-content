55ba9dad9158176b0838e548fadcd4fe4acec046
Philip <philipspan@gmail.com>
2020-02-17 17:04:08 -0800

    Added modes and separate development and production configs.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,55 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const json = require('./src/client/js/file.json');
+
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+
+		mode: 'production',
+		entry: './src/client/index.js',
+		module: {
+            
+            rules: [
+                    {
+                		enforce: 'pre',
+                		test: '/\.js$/',
+                		exclude: /node_modules/,
+                		loader: "eslint-loader"
+                    },
+                    {
+                    	test: '/\.js$/',
+                		exclude: /node_modules/,
+                		loader: "babel-loader",
+                    },
+                    {
+                    	test: /\.(png|jpe?g|gif)$/i,
+                    	exclude: /node_modules/,
+        				use: [
+          					{
+            					loader: 'file-loader',
+          					}
+          				]
+                    },
+                    {
+                    	test: '/\.json$/',
+        				loader: "json-loader"
+                    }
+
+            ]
+
+    	},
+    	plugins: [
+    		new HtmlWebpackPlugin({
+
+    			template: './src/client/views/index.html',
+    			filename: './index.html',
+
+    		})
+    	]
+
+};
+
+
+
+
